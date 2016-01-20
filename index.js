75cc32f257b497bf9fe8862e83c994826c6bf43e
lorenzh <lorenz.hilpert@gmail.com>
2016-01-20 11:11:25 +0100

    add save and load cookiejar from file

--- index.js
+++ index.js
@@ -2,12 +2,45 @@
 /* jshint strict: true */
 "use strict";
 
-module.exports = function(apiOptions){
-  var login = new (require("./lib/login"))(apiOptions);
+module.exports = function(options){
+  var utils = require("./lib/utils");
+  var fs = require("fs");
+  var defaultOptions = {
+      saveCookie: false,
+      saveCookiePath: null,
+      loadCookieFromSavePath: false,
+      cookieJarJson: null      
+  };
+  utils.mergeOptions(defaultOptions,options);
+  var login = new (require("./lib/login"))(options);
 
   var loginResponse = {};
 
-  var futApi = function(){};
+  var futApi = function(){
+      if(defaultOptions.loadCookieFromSavePath)
+      {
+          try  {
+            fs.accessSync(defaultOptions.saveCookiePath, fs.R_OK | fs.W_OK);
+            var jsonString = fs.readFileSync(defaultOptions.saveCookiePath,"utf8");
+            login.setCookieJarJSON(JSON.parse(jsonString));
+          }
+          catch(e){  }
+      }
+      else if(defaultOptions.cookieJarJson && typeof defaultOptions.cookieJarJson == "object")
+      {
+          var dcj = defaultOptions.cookieJarJson;
+          if(dcj.version && dcj.storeType && dcj.rejectPublicSuffixes && dcj.cookies)
+            login.setCookieJarJSON(dcj);
+      }
+  };
+  
+  futApi.prototype.getCookieJarJSON = function(){
+    return login.getCookieJarJSON();
+  };
+
+  futApi.prototype.setCookieJarJSON = function(json){
+      login.setCookieJarJSON(json);
+  };
 
   futApi.prototype.login = function(email, password, secret, tfCodeCb, loginCb){
     login.login(email, password, secret, tfCodeCb, function(error, result){
@@ -15,11 +48,19 @@ module.exports = function(apiOptions){
         loginCb(error);
       else {
         loginResponse = result;
-        loginCb(null, result);
+        
+        if(defaultOptions.saveCookie && defaultOptions.saveCookiePath)
+        {
+            fs.writeFile(defaultOptions.saveCookiePath, 
+                JSON.stringify(login.getCookieJarJSON()), 
+                "utf8", 
+                function(saveError){if(saveError) throw saveError;});
+        }
+        
+        loginCb(null, result);        
       }
     });
   };
-
 
   return new futApi();
 };
